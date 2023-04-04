6fc38371341010b7dc67a8bb15b685e8d9ce45f5
v1nizin <viniwick@outlook.com>
2023-04-04 16:04:47 -0300

    Criação model Livro

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Autor, Categoria, Editora
+from .models import Autor, Categoria, Livro 
 
 admin.site.register(Autor)
 admin.site.register(Categoria)
 admin.site.register(Editora)
+admin.site.register(Livro)
